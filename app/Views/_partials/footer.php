20944647443cf2406dfcc3e181a910d6908cfae1
Elang Surya <elangsurya19@gmail.com>
2020-07-04 02:38:21 +0700

    first commit

---
+++ app/Views/_partials/footer.php
@@ -0,0 +1,21 @@
+<aside class="control-sidebar control-sidebar-dark">
+    <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+    </div>
+</aside>
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+        Sistem Informasi Penjualan
+    </div>
+    <strong>Copyright © 2014-2019 <a href="<?php echo base_url('/'); ?>">SIP Online</a>.</strong> All rights reserved.
+</footer>
+</div>
+
+<script src="<?php echo base_url('themes/adminlte/plugins'); ?>/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url('themes/adminlte/plugins'); ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url('themes/adminlte/dist'); ?>/js/adminlte.min.js"></script>
+</body>
+
+</html>
