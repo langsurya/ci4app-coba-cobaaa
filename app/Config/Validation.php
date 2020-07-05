f3702ba6c2d3bd5b6cb9a40b82e1e7b0b83347a4
Elang Surya <elangsurya19@gmail.com>
2020-07-06 03:27:24 +0700

    module product

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -48,4 +48,37 @@ class Validation
 			'required' => 'Status category wajib diisi.'
 		]
 	];
+
+	public $product = [
+		'category_id'			=> 'required',
+		'product_name'			=> 'required',
+		'product_price'			=> 'required',
+		'product_sku'			=> 'required',
+		'product_status'		=> 'required',
+		'product_image'			=> 'uploaded[product_image]|mime_in[product_image,image/jpg,image/jpeg,image/gif,image/png]|max_size[product_image,1000]',
+		'product_description'	=> 'required'
+	];
+
+	public $product_errors = [
+		'category_id'	=> [
+			'required'	=> 'Nama category wajib diisi.',
+		],
+		'product_name'	=> [
+			'required'	=> 'Nama product wajib diisi.'
+		],
+		'product_sku'	=> [
+			'required'	=> 'Kode product wajib diisi.'
+		],
+		'product_status' => [
+			'required'	=> 'Status product wajib diisi.'
+		],
+		'product_image' => [
+			'mime_in'	=> 'Gambar product hanya boleh diisi dengan jpg, jpeg, png atau gif.',
+			'max_size'	=> 'Gambar product maksimal 1mb',
+			'uploaded'	=> 'Gambar product wajib diisi'
+		],
+		'product_description' => [
+			'required'	=> 'Desacription product wajib diisi.'
+		]
+	];
 }
